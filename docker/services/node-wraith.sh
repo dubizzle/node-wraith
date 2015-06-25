756e0cbd8ff98b86f2caaea613369bbea39f14d0
Mahmoud Felfel <mahmoudfelfel@gmail.com>
2015-06-25 12:12:50 +0400

    fix service name variable

--- docker/services/node-wraith.sh
+++ docker/services/node-wraith.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+chmod a+r /etc/container_environment.sh
+source /etc/container_environment.sh
 
-exec /sbin/setuser dubizzle node $REPO_DIR/cli.js --config $REPO_DIR/config/chrome.json  2&>1 $LOG_DIR/$SERVICE_NAME.log
+exec /sbin/setuser dubizzle nodejs $REPO_DIR/cli.js --config $REPO_DIR/config/chrome.json  2>&1 $LOG_DIR/$SERVICE_NAME.log
