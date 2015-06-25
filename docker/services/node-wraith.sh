050316f6a2f47c1b0a5a45a36405c7996f7dc762
Mahmoud Felfel <mahmoudfelfel@gmail.com>
2015-06-25 15:15:39 +0400

    chdir to REPO_DIR

--- docker/services/node-wraith.sh
+++ docker/services/node-wraith.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 chmod a+r /etc/container_environment.sh
 source /etc/container_environment.sh
-
-exec /sbin/setuser dubizzle nodejs $REPO_DIR/cli.js --config $REPO_DIR/config/chrome.json  2>&1 $LOG_DIR/$SERVICE_NAME.log
+cd $REPO_DIR
+/sbin/setuser dubizzle npm install
+exec /sbin/setuser dubizzle nodejs $REPO_DIR/cli.js --config $REPO_DIR/config/chrome.json  2>&1 $USER_LOGGING_DIR/$SERVICE_NAME.log
